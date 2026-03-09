3e5d6bb049b8603c4047671e21af7233426acf47
Salvador Reynoso <salvareyvillaverde@gmail.com>
2026-03-09 05:25:18 -0700

    Actualizacion de practica 1

--- parcial-1-poo/practica-1/index.php
+++ parcial-1-poo/practica-1/index.php
@@ -1,2 +1,11 @@
 <?php
-echo "hola";
+
+require_once "Usuario.php";
+
+$Usuario = new Usuario("Maria", "maria@correo.com");
+echo "<h2>Datos del usuario</h2>";
+
+echo "Nombre: " . $Usuario->getNombre() . "<br>";
+echo "Correo: " . $Usuario->getCorreo() . "<br>";
+
+?> 
